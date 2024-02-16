2e2b1a007e2eceb8780c2e185d10a33b52be7757
EXCEPTION <142222853+SexyException@users.noreply.github.com>
2024-02-16 22:49:53 +0530

    Update __main__.py
    Signed-off-by: EXCEPTION <142222853+SexyException@users.noreply.github.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await Anony.start()
     try:
-        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Anony.stream_call("https://graph.org/file/228c36d54ca891d0310f5.mp4")
     except NoActiveGroupCall:
         LOGGER("AnonXMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
